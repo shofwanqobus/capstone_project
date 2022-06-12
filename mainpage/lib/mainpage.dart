85bd8ab315a76dcbe7062a85c69214fe03556ba0
Muhammad Shofwan Qobus <shofwanqobus@upi.edu>
2022-06-12 16:50:19 +0700

    update

--- mainpage/lib/mainpage.dart
+++ mainpage/lib/mainpage.dart
@@ -2,15 +2,13 @@ library mainpage;
 
 export 'presentation/pages/home_page.dart';
 export 'presentation/pages/details/detail_screen.dart';
-export 'presentation/pages/details/payment_screen.dart';
-export 'presentation/pages/details/payment_succeed.dart';
-export 'presentation/pages/details/trip_detail.dart';
+export 'presentation/pages/payment_screen.dart';
+export 'presentation/pages/payment_succeed.dart';
 export 'presentation/pages/search_page.dart';
 export 'presentation/pages/booked_page.dart';
 export 'presentation/pages/user_page.dart';
 export 'presentation/pages/settings_page.dart';
-export 'presentation/pages/tickets/ticket_screen.dart';
-export 'presentation/pages/tickets/ticket_list.dart';
+export 'presentation/pages/tickets/plane.dart';
 
 export 'presentation/pages/settings/language.dart';
 export 'presentation/pages/settings/theme.dart';
